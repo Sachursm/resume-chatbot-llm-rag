f5fa43cd015e8c6e871aa48c5981748d66896695
Sachursm <sachuretnasm@gmail.com>
2025-12-22 22:44:11 +0530

    created code to create chunk from the text extracted from the pdf file inside scripts folder

---
+++ scripts/chunking.py
@@ -0,0 +1,10 @@
+def chunk_by_lines(text: str, max_lines: int = 3) -> list[str]:
+    lines = text.split("\n")
+    chunks = []
+
+    for i in range(0, len(lines), max_lines):
+        chunk = "\n".join(lines[i:i + max_lines]).strip()
+        if chunk:
+            chunks.append(chunk)
+
+    return chunks
